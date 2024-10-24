9f29bd6f5077016ba427f65a2a9d8f9b0fe8019c
ka1te6 <a.andrey2006@yandex.ru>
2024-10-25 01:55:32 +0300

    No changes

--- laba32.cpp
+++ laba32.cpp
@@ -1,43 +1,89 @@
 #include <iostream>
-#include <cstdlib> 
-#include <ctime>  
+#include <cstdlib>
+#include <ctime>
 
 int main() {
-
     setlocale(LC_ALL, "Russian");
 
-    srand(static_cast<unsigned>(time(0)));  // Инициализация генератора случайных чисел
+    srand(static_cast<unsigned>(time(0)));
 
-    int size;
-    std::cout << "Введите размер массива: ";
-    std::cin >> size;
+    int n, m, l;
+    std::cout << "Введите количество строк первой матрицы (n): ";
+    std::cin >> n;
+    std::cout << "Введите количество столбцов первой матрицы (и строк второй матрицы) (m): ";
+    std::cin >> m;
+    std::cout << "Введите количество столбцов второй матрицы (l): ";
+    std::cin >> l;
 
-    if (size <= 0) {
-        std::cout << "Некорректный размер массива" << std::endl;
-        return 1;
+    int** A = new int* [n];
+    int** B = new int* [m];
+    int** C = new int* [n];
+
+    for (int i = 0; i < n; ++i) {
+        A[i] = new int[m];
+    }
+    for (int i = 0; i < m; ++i) {
+        B[i] = new int[l];
+    }
+    for (int i = 0; i < n; ++i) {
+        C[i] = new int[l];
     }
 
-    // Выделяем память под массив в куче
-    int* array = new int[size];
-
-    std::cout << "Исходный массив: ";
-    for (int i = 0; i < size; ++i) {
-        array[i] = rand() % 100;  // Заполняем массив случайными числами
-        std::cout << array[i] << " ";
+    std::cout << "Матрица A:\n";
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < m; ++j) {
+            A[i] = rand() % 10;
+            std::cout << A[i][j] << " ";
+        }
+        std::cout << std::endl;
     }
-    std::cout << std::endl;
 
-    // Вычисляем среднее арифметическое
-    int sum = 0;
-    for (int i = 0; i < size; ++i) {
-        sum += array[i];
+    std::cout << "Матрица B:\n";
+    for (int i = 0; i < m; ++i) {
+        for (int j = 0; j < l; ++j) {
+            B[i][j] = rand() % 10;
+            std::cout << B[i][j] << " ";
+        }
+        std::cout << std::endl;
     }
-    double average = static_cast<double>(sum) / size;
 
-    std::cout << "Среднее арифметическое: " << average << std::endl;
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < l; ++j) {
+            C[i][j] = 0;
+        }
+    }
 
-    // Освобождаем память
-    delete[] array;
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < l; ++j) {
+            for (int k = 0; k < m; ++k) {
+                C[i][j] += A[i][k] * B[k][j];
+            }
+        }
+    }
+
+    std::cout << "Результат произведения матриц A и B (матрица C):\n";
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < l; ++j) {
+            std::cout << C[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
+
+    for (int i = 0; i < n; ++i) {
+        delete[] A[i];
+    }
+    delete[] A;
+
+    for (int i = 0; i < m; ++i) {
+        delete[] B[i];
+    }
+    delete[] B;
+
+    for (int i = 0; i < n; ++i) {
+        delete[] C[i];
+    }
+    delete[] C;
 
     return 0;
 }
+
